fe9daf25f0d752933a52942467468e82a462cc59
bdavis171 <bdavis171@gmail.com>
2020-03-03 14:25:52 -0600

    work finished

--- ex1.js
+++ ex1.js
@@ -5,4 +5,23 @@ console.log('ex1.js loaded');
 let oneBtn = document.querySelector('#one');
 //Create a variable for the '2' button
 let twoBtn = document.querySelector('#two');
+//Create a variable for the header title
+let title = document.querySelector('#title');
 
+//Create a function to change the header title to 1 and the background color to blue
+function changeOne(event) {
+    title.innerHTML = '1';
+    title.classList.toggle('blueBackground');
+}
+
+//Add an event to the '1' button containing the above function
+oneBtn.addEventListener('click',changeOne);
+
+//Create a function to change the header title to 2 and the background color to red
+function changeTwo(event){
+    title.innerHTML = '2';
+    title.classList.toggle('redBackground');
+}
+
+//Add an event to the '2' button containing the above function
+twoBtn.addEventListener('click',changeTwo);
