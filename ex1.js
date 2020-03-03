75de53162b4673b649f4fd58fa9635c252e8e4fe
bdavis171 <bdavis171@gmail.com>
2020-03-03 14:14:16 -0600

    initial check in

---
+++ ex1.js
@@ -0,0 +1,8 @@
+// sanity
+console.log('ex1.js loaded');
+
+//Create a variable to store the '1' button
+let oneBtn = document.querySelector('#one');
+//Create a variable for the '2' button
+let twoBtn = document.querySelector('#two');
+
